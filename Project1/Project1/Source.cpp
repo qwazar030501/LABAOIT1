9206e60c0b9f9859763a6af84b51981c4acd0b20
qwazar030501 <faraondk34@mail.ru>
2025-11-14 09:23:56 +0300

    5 commit

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -5,4 +5,11 @@ void main() {
         //jfdfjdfj
         //ajsdjsad
     printf("vetkavetka");
+    printf("     _____\n");
+    printf("  .-\"     \"-.\n");
+    printf(" /          \\\n");
+    printf("|  O     O   |\n");
+    printf("|     ^      |\n");
+    printf(" \\ /___/    /\n");
+    printf("  '-.___.-'\n");
 }
