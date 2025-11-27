ce9d22d41b910e3440f9c563000987e262e2325b
qwazar030501 <faraondk34@mail.ru>
2025-11-27 22:10:56 +0400

    commit vetka2

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,20 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <Windows.h>
 
-int fibonacci(int n) {
-    if (n <= 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+void main() {
+	printf("hello world");
+	printf("hello world123");
 }
-
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    int n;
-    printf("число фибоначи: ");
-    scanf("%d", &n);
-    int res = fibonacci(n);
-    printf("(%d) = %d\n", n, res);
-}
-
-
