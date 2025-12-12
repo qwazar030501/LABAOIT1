f0ac6281d7f2f975daf991cb12bdb56abc13f6ff
qwazar030501 <faraondk34@mail.ru>
2025-12-12 09:16:11 +0300

    commit23

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -2,31 +2,6 @@
 #include <stdio.h>
 #include <Windows.h>
 
-int fibonacci(int n) {
-    if (n <= 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+void main() {
+	printf("hello world");
 }
-
-int main() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    int n;
-    printf("число фибоначи: ");
-    scanf("%d", &n);
-    int res = fibonacci(n);
-    printf("(%d) = %d\n", n, res);
-    printf("sf");
-    printf("sf");
-    printf("sf");
-    printf("sf");
-    printf("sf2");
-    printf("sf2");
-    printf("sf2");
-    printf("sf3");
-    printf("sf3");
-    printf("sf3");
-    printf("sf4");
-
-}
-
-
